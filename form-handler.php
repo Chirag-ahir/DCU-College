63cba33c95debde7912cf88f8eba2542089fd7ad
AHIR CHIRAG <72852725+Chirag-ahir@users.noreply.github.com>
2022-03-31 12:08:23 +0530

    Add files via upload

---
+++ form-handler.php
@@ -0,0 +1,20 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject']; 
+$message = $_POST['message'];
+                
+$email_from = 'info@dcuPvtLtd.com';
+$email_subject = 'New Form Submission'; 
+$email_body = "User Name: $name. \n".
+                "User Email: $visitor_email.\n".
+                 "Subject: $subject.\n".
+                 "User Message: $message .\n";
+
+$to = 'ahirchirag8125@gmail.com';
+$headers = "From: $email_from \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
+mail($to,$email_subject,$email_body,$headers);
+header ("Location: contact.html");
+
+?>
